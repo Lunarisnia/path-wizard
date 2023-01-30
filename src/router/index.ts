2c5f050b2325f029a39dbc4d6a8fba5cdc36a03a
Loona <r.arswendo.r@gmail.com>
2023-01-30 10:46:34 +0700

    separated pathWiz from home route

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/pathwiz',
+        name: 'Home',
+        component: () => import('@/views/PathWiz.vue'),
+      },
     ],
   },
 ]
---
+++ src/views/PathWiz.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container style="background-color: black; height: 100%; width: 100%;" fluid>
+    <Grid :col="col" :row="row" :debug="false" />
+  </v-container>
+</template>
+
+<script lang="ts">
+import Grid from '@/components/Grid.vue';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: {
+    Grid
+  },
+  data() {
+    return {
+      row: 4,
+      col: 5
+    }
+  }
+})
+</script>
